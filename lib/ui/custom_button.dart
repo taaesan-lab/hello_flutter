08cef1892bdc2a8c4ba11d4e3cb1412b3b10ebae
taaesan <taaesan@gmail.com>
2021-06-28 05:23:27 +0700

    Add custom button

---
+++ lib/ui/custom_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(content: Text('Hi There!'));
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: Colors.amber, borderRadius: BorderRadius.circular(8.0)),
+        child: Text('Button'),
+      ),
+    );
+  }
+}
