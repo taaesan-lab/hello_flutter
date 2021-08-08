da9a6bae26b2f720253a4a159c3fc468fedf99e2
taaesan <taaesan@gmail.com>
2021-08-08 14:11:44 +0700

    Update colors.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/ui/day6/movie_list.dart';
-import 'package:hello_flutter/ui/day4/quote_of_the_day.dart';
+// import 'package:hello_flutter/ui/day4/quote_of_the_day.dart';
+import 'package:hello_flutter/ui/day8/mortgage_app.dart';
 
 // import 'ui/split-calculator.dart';
 // import 'ui/quote_of_the_day.dart';
 // import 'ui/random.dart';
 // import 'ui/scaffold_example.dart';
-import 'ui/day7/custom_theme.dart';
+// import 'ui/day7/custom_theme.dart';
 
 
 void main() => runApp(new MaterialApp(
@@ -17,7 +18,7 @@ void main() => runApp(new MaterialApp(
       // ),
       // home: RandomWords()
       // home: ScaffoldExample()
-      home: QuoteWidget(),
-      // home: MovieListWidget(),
+      // home: MortgageApp(),
+      home: MovieListWidget(),
       debugShowCheckedModeBanner: false,
     ));
